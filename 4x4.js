b9c4b6c98c4a5cc2ff14ef33e5792d3a8dc285c3
Katya <katya.mikhaylova.98@list.ru>
2023-04-06 04:25:58 +0200

    add: drawing color matrix 4x4

--- 4x4.js
+++ 4x4.js
@@ -1,3 +1,20 @@
 const canvas = document.getElementById('myCanvas')
 const ctx = canvas.getContext('2d')
+canvas.width = 512
+canvas.height = 512
 
+fetch('./4x4.json')
+  .then(response => response.json())
+  .then(data => {
+    const colors = data
+    const squareSize = 128
+    const rows = canvas.height / squareSize
+    const cols = canvas.width / squareSize
+
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
+        ctx.fillStyle = '#' + colors[row][col]
+        ctx.fillRect(col * squareSize, row * squareSize, squareSize, squareSize)
+      }
+    }
+  })
