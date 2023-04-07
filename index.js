23283baa5a4af579eb48aa9fa19af8fe32316a23
Katya <katya.mikhaylova.98@list.ru>
2023-04-07 03:02:36 +0200

    add: onClick handler on element switcher

--- index.js
+++ index.js
@@ -1,43 +1,66 @@
-const canvas = document.getElementById('myCanvas')
-const ctx = canvas.getContext('2d')
+const options = document.querySelectorAll('.switcher__option-square')
 
-canvas.width = 512
-canvas.height = 512
+options.forEach(option => {
+  option.addEventListener('click', () => {
+    options.forEach(option => option.classList.remove('selected'))
+    console.log(option.classList.add('selected'))
+    if (option.classList.contains('size-four') && option.classList.contains('selected')) {
+      const canvas = document.getElementById('myCanvas')
+      const ctx = canvas.getContext('2d')
 
-// fetch('./4x4.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     const colors = data
-//     const squareSize = 128
-//     const rows = canvas.height / squareSize
-//     const cols = canvas.width / squareSize
+      canvas.width = 512
+      canvas.height = 512
 
-//     for (let row = 0; row < rows; row++) {
-//       for (let col = 0; col < cols; col++) {
-//         ctx.fillStyle = '#' + colors[row][col]
-//         ctx.fillRect(col * squareSize, row * squareSize, squareSize, squareSize)
-//       }
-//     }
-//   })
+      fetch('./4x4.json')
+        .then(response => response.json())
+        .then(data => {
+          const colors = data
+          const squareSize = 128
+          const rows = canvas.height / squareSize
+          const cols = canvas.width / squareSize
 
-// fetch('./32x32.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     const colors = data
-//     const squareSize = 16
-//     const rows = canvas.height / squareSize
-//     const cols = canvas.width / squareSize
+          for (let row = 0; row < rows; row++) {
+            for (let col = 0; col < cols; col++) {
+              ctx.fillStyle = '#' + colors[row][col]
+              ctx.fillRect(col * squareSize, row * squareSize, squareSize, squareSize)
+            }
+          }
+        })
+    } else if (option.classList.contains('size-thirty-two') && option.classList.contains('selected')) {
+      const canvas = document.getElementById('myCanvas')
+      const ctx = canvas.getContext('2d')
 
-//     for (let row = 0; row < rows; row++) {
-//       for (let col = 0; col < cols; col++) {
-//         ctx.fillStyle = `rgba(${colors[row][col]})`
-//         ctx.fillRect(col * squareSize, row * squareSize, squareSize, squareSize)
-//       }
-//     }
-//   })
+      canvas.width = 512
+      canvas.height = 512
 
-const img = new Image()
-img.src = './assets/image.png'
-img.onload = function () {
-  ctx.drawImage(img, 0, 0, 512, 512)
-}
+      fetch('./32x32.json')
+        .then(response => response.json())
+        .then(data => {
+          const colors = data
+          const squareSize = 16
+          const rows = canvas.height / squareSize
+          const cols = canvas.width / squareSize
+
+          for (let row = 0; row < rows; row++) {
+            for (let col = 0; col < cols; col++) {
+              ctx.fillStyle = `rgba(${colors[row][col]})`
+              ctx.fillRect(col * squareSize, row * squareSize, squareSize, squareSize)
+            }
+          }
+        })
+    } else if (option.classList.contains('size-img') && option.classList.contains('selected')) {
+      const canvas = document.getElementById('myCanvas')
+      const ctx = canvas.getContext('2d')
+
+      canvas.width = 512
+      canvas.height = 512
+      const img = new Image()
+      img.src = './assets/image.png'
+      img.onload = function () {
+        ctx.drawImage(img, 0, 0, 512, 512)
+      }
+
+    }
+  })
+})
+
